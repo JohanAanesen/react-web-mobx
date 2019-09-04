d707f2cd5987f1a590185bbad98e0b4dfbf16509
Johan Aanesen <johan.aanesen@inmeta.no>
2019-09-04 14:18:45 +0200

    adding project

--- src/App.js
+++ src/App.js
@@ -1,26 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import Router from "./router";
+import { TodoProvider } from "./todos/todo-context";
+import { BrowserRouter } from "react-router-dom";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<TodoProvider>
+			<BrowserRouter>
+				<div className="App">
+					{/* navbar n shi goes here :):):):) :^) */}
+					<Router />
+				</div>
+			</BrowserRouter>
+		</TodoProvider>
+	);
 }
 
 export default App;
